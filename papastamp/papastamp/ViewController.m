a62e9903d3b1b6efef4c911151f8f8bfeee25dae
marbean <marbeankim@gmail.com>
2017-10-31 01:02:45 +0900

    first commit

---
+++ papastamp/papastamp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  papastamp
+//
+//  Created by 김대현 on 2017. 10. 31..
+//  Copyright © 2017년 김대현. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
